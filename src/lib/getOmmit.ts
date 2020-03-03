9be12e0f2f5ac02994263aa578b97c081c1f3aca
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-03 23:51:46 +0900

    {add]

---
+++ src/lib/getOmmit.ts
@@ -0,0 +1,6 @@
+export const getOmmit = (target: string) => {
+  return target.slice(
+    target.search("\n") + 1,
+    target.replace("##", "").search("##")
+  ).replace(/\!\[.*\]\(.*\)/g,"");
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Layout } from "../components/Layout";
 import { Entry } from "../lib/getBlogPost";
 import { Top } from "../components/Top";
+import { Item } from "../components/Item";
 
 const Home = (props: { items: Entry[] }) => {
   return (
     <div>
       <Top />
-      {props.items.map((en, index) => {
-        return <div key={index}>{en.fields.title}</div>;
+      {props.items.map((item, index) => {
+        return (
+          <div key={index}>
+            <Item {...item.fields} />
+          </div>
+        );
       })}
     </div>
   );
