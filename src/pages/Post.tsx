fdab29e64818b9fb5f52018e11364a59cc5733d8
svpcadmin <PhilisPaxil@gmail.com>
2020-05-04 16:00:23 +0900

    fix: profile

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -4,20 +4,29 @@ import parse from "html-react-parser";
 import { Layout } from "../components/Layout";
 import { Entry } from "../lib/getBlogPost";
 import { MetaInfo } from "../components/MetaInfo";
-import ThatsMe from "../components/ThatsMe";
 import { Anchor } from "../components/Anchor";
+import { BaseProfile } from "../components/BaseProfile";
+import { MyIcon } from "../components/MyIcon";
 
 interface Props extends Entry {
-  anchors: string[]
+  anchors: string[];
 }
 
 function Post(props: Props) {
   return (
     <div className="post--content">
-      <Anchor anchors={props.anchors}/>
+      <Anchor anchors={props.anchors} />
       <MetaInfo {...props} />
       {parse(props.fields.content)}
-      <ThatsMe />
+
+      <MyIcon
+        styles={{
+          height: "200px",
+          width: "200px",
+          margin: "12px auto",
+        }}
+      />
+      <BaseProfile />
     </div>
   );
 }
