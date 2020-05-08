eb5c3f0a0657bcb9eb1d800b8dc73da8fa863185
svpcadmin <PhilisPaxil@gmail.com>
2020-05-08 14:12:25 +0900

    add: module

--- scripts/perf.ts
+++ scripts/perf.ts
@@ -1,8 +1,10 @@
 import { flagParser } from "./flagParser"
+import { context } from "@actions/github"
 
 const main = () => {
     const flags = flagParser();
     console.log(flags.target)
+    console.log(context?.payload?.pull_request?.head.ref)
 }
 
 main()
