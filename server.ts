eba088da4e96143d19699c5b82a5a37ee5de2da3
svpcadmin <PhilisPaxil@gmail.com>
2020-05-17 02:00:32 +0900

    fix:server

--- server.ts
+++ server.ts
@@ -92,6 +92,299 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/json", (req, res) => {
+  const slug = getHomeSlug(req.req.url || "");
+  getBlogPosts(slug).then((items) => {
+    res.headers({
+      "content-type": "application/json",
+    });
+    res.send(JSON.stringify(items));
+  });
+});
+
+app.get("/post/:id", (req, res) => {
+  const slug = getBlogSlug(req.req.url || "");
+  getBlogPost(slug).then((item) => {
+    if (!item) {
+      const renderedHtml = renderToString(
+        React.createElement(
+          helmet({
+            title: `Not Found | ${TITLE}`,
+            children: Post,
+            style: "post",
+            slug: "https://shinyaigeek.dev",
+            props: {
+              fields: {
+                title: "Not Found",
+                description: "item is not found",
+                slug: "none",
+                publishedAt: "0000/00/00",
+                tags: [],
+                content: "Item is not found",
+                hasEn: false,
+              },
+              anchors: [],
+            },
+          })
+        )
+      );
+      res.headers({
+        "content-type": "text/html; charset=utf-8",
+      });
+      res.send(renderedHtml);
+    } else {
+      marked.setOptions({
+        langPrefix: "",
+        highlight: (code, lang) => {
+          return hljs.highlightAuto(code, [lang]).value;
+        },
+      });
+      const html = marked(item.fields.content);
+      const anchorsWithH2: string[] | null = html.match(
+        /<h2 id=".+?">.+?<\/h2>/g
+      );
+      let anchors;
+      if (anchorsWithH2) {
+        anchors = anchorsWithH2.map((anc) => {
+          return anc.replace(/<h2 id=".+?">/, "").replace("</h2>", "");
+        });
+      }
+      const body = html.replace(/<h2 id=".+?">/g, (target: string) => {
+        const id = target.replace('<h2 id="', "").replace('">', "");
+        return `<h2 id="${encodeURI(id)}">`;
+      });
+      const pro = {
+        fields: {
+          title: item.fields.title,
+          description: item.fields.description,
+          tags: item.fields.tags,
+          publishedAt: item.fields.publishedAt,
+          hasEn: item.fields.hasEn,
+          content: body,
+          slug: item.fields.slug,
+        },
+        anchors: anchors,
+      };
+      const renderedHtml = renderToString(
+        React.createElement(
+          helmet({
+            title: `${pro.fields.title} | ${TITLE}`,
+            style: "post",
+            slug: `https://shinyaigeek.dev/${pro.fields.slug}`,
+            children: Post,
+            props: pro,
+          })
+        )
+      );
+      res.headers({
+        "content-type": "text/html; charset=utf-8",
+      });
+      res.send(renderedHtml);
+    }
+  });
+});
+
+app.get("/profile", (req, res) => {
+  const renderedHtml = renderToString(
+    React.createElement(
+      helmet({
+        title: `Profile | ${TITLE}`,
+        style: "profile",
+        children: Profile,
+        slug: "https://shinyaigeek.dev/profile",
+      })
+    )
+  );
+  res.headers({
+    "content-type": "text/html; charset=utf-8",
+  });
+  res.send(renderedHtml);
+});
+
+app.put("/withItems", (req, res) => {
+  try {
+    const { rawItems } = req.body;
+    const items = JSON.parse(rawItems);
+    if (!items || items.items.length === 0) {
+      const renderedHtml = renderToString(
+        React.createElement(
+          helmet({
+            title: TITLE,
+            children: Home,
+            style: "home",
+            slug: "https://shinyaigeek.dev/",
+            props: {
+              items: [
+                {
+                  fields: {
+                    title: "Not Found",
+                    description: "item is not found",
+                    slug: "none",
+                    publishedAt: "0000/00/00",
+                    tags: [],
+                    content: "Item is not found",
+                    hasEn: false,
+                  },
+                },
+              ],
+            },
+          })
+        )
+      );
+      res.headers({
+        "content-type": "text/html; charset=utf-8",
+      });
+      res.send(renderedHtml);
+    } else {
+      const renderedHtml = renderToString(
+        React.createElement(
+          helmet({
+            title: TITLE,
+            children: Home,
+            slug: "https://shinyaigeek.dev/",
+            style: "home",
+            props: {
+              items: items.items,
+              prev: items.prev,
+              next: items.next,
+            },
+          })
+        )
+      );
+      res.headers({
+        "content-type": "text/html; charset=utf-8",
+      });
+      res.send(renderedHtml);
+    }
+  } catch (_) {
+    console.log(_);
+    res.send("oops");
+  }
+});
+
+app.put("/renderWithItem", (req, res) => {
+  console.log("--------------------------");
+  console.log(
+    "=========== access post to render with Data Json =============="
+  );
+  try {
+    const { rawItem } = req.body;
+    const item = JSON.parse(rawItem).items[0];
+    if (!item) {
+      const renderedHtml = renderToString(
+        React.createElement(
+          helmet({
+            title: `Not Found | ${TITLE}`,
+            children: Post,
+            style: "post",
+            slug: "https://shinyaigeek.dev",
+            props: {
+              fields: {
+                title: "Not Found",
+                description: "item is not found",
+                slug: "none",
+                publishedAt: "0000/00/00",
+                tags: [],
+                content: "Item is not found",
+                hasEn: false,
+              },
+              anchors: [],
+            },
+          })
+        )
+      );
+      res.headers({
+        "content-type": "text/html; charset=utf-8",
+      });
+      res.send(renderedHtml);
+    } else {
+      marked.setOptions({
+        langPrefix: "",
+        highlight: (code, lang) => {
+          return hljs.highlightAuto(code, [lang]).value;
+        },
+      });
+      const html = marked(item.fields.content);
+      const anchorsWithH2: string[] | null = html.match(
+        /<h2 id=".+?">.+?<\/h2>/g
+      );
+      let anchors;
+      if (anchorsWithH2) {
+        anchors = anchorsWithH2.map((anc) => {
+          return anc.replace(/<h2 id=".+?">/, "").replace("</h2>", "");
+        });
+      }
+      const body = html.replace(/<h2 id=".+?">/g, (target: string) => {
+        const id = target.replace('<h2 id="', "").replace('">', "");
+        return `<h2 id="${encodeURI(id)}">`;
+      });
+      const pro = {
+        fields: {
+          title: item.fields.title,
+          description: item.fields.description,
+          tags: item.fields.tags,
+          publishedAt: item.fields.publishedAt,
+          hasEn: item.fields.hasEn,
+          content: body,
+          slug: item.fields.slug,
+        },
+        anchors: anchors,
+      };
+      const renderedHtml = renderToString(
+        React.createElement(
+          helmet({
+            title: `${pro.fields.title} | ${TITLE}`,
+            style: "post",
+            slug: `https://shinyaigeek.dev/${pro.fields.slug}`,
+            children: Post,
+            props: pro,
+          })
+        )
+      );
+      res.headers({
+        "content-type": "text/html; charset=utf-8",
+      });
+      res.send(renderedHtml);
+    }
+  } catch (_) {
+    console.log(_);
+    res.send("oops");
+  }
+});
+
+app.get("/getSitemap", (req, res) => {
+  try {
+    getSiteMap()
+      .then((xml) => {
+        res.headers({
+          "content-type": "text/xml; charset=utf-8",
+        });
+        res.send(xml);
+      })
+      .catch((e) => {
+        throw new Error();
+      });
+  } catch (e) {
+    res.send("oops");
+  }
+});
+
+app.get("/getRss", (req, res) => {
+  try {
+    getRss()
+      .then((xml) => {
+        res.headers({
+          "content-type": "text/xml; charset=utf-8",
+        });
+        res.send(xml);
+      })
+      .catch((e) => {
+        throw new Error();
+      });
+  } catch (e) {
+    res.send("oops");
+  }
+});
 // Run the server!
 app.listen(3000, (err, address) => {
   if (err) throw err;
