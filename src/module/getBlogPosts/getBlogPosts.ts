66a37d96fccdb9935e9bc5537355d98097f5edba
svpcadmin <PhilisPaxil@gmail.com>
2020-08-15 07:46:38 +0900

    add: getBlogPosts

--- src/module/getBlogPosts/getBlogPosts.ts
+++ src/module/getBlogPosts/getBlogPosts.ts
@@ -1,12 +1,13 @@
 import { HomeSlug } from "../getHomeSlug/getHomeSlug"
 import { Meta } from "../../utils/makeIndex/makeIndex"
 import assert from "assert"
+import { findBlogPosts } from "./findBlogPosts"
 
 export const getBlogPosts: (slug: HomeSlug) => Promise<Meta[]> = (slug) => {
     assert(process.env.BLOG_POSTS_INDEX_URL, "oops !! there is no BLOG_POSTS_INDEX_URL. please check env.")
     return fetch(process.env.BLOG_POSTS_INDEX_URL).then(res => {
         return res.json().then((json: Meta[]) => {
-            
+            return findBlogPosts(json, slug);
         })
     })
 }
