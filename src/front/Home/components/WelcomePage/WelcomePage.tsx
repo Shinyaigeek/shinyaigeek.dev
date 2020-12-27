a5a645c701d7e460c5b1a231ef17571577cc5a6d
shinyaigeek <PhilisPaxil@gmail.com>
2020-12-27 14:40:10 +0900

    feature(styling): styleing welcome page

--- src/front/Home/components/WelcomePage/WelcomePage.tsx
+++ src/front/Home/components/WelcomePage/WelcomePage.tsx
@@ -2,10 +2,33 @@ import { ShinyaigeekAnimation } from "../../../components/ShinyaigeekAnimation/S
 import { ScrollDown } from "../ScrollDown/ScrollDown";
 import { StarStream } from "../StartStream/StarStream";
 import React from "react";
+import { css } from "linaria";
+
+const welcome = css`
+  height: calc(100vh - 72px);
+  width: 100vw;
+  position: relative;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 999;
+`;
+
+const banana = css`
+  height: 200px;
+  margin: 20px 20px;
+  object-fit: contain;
+`;
+
 export function WelcomePage() {
   return (
-    <div className="welcome">
-      <img className="banana" src="https://storage.cloud.google.com/blog_assets_shinyaigeek/static/banana.png" alt="banana" />
+    <div className={welcome}>
+      <img
+        className={banana}
+        src="https://storage.cloud.google.com/blog_assets_shinyaigeek/static/banana.png"
+        alt="banana"
+      />
       <ScrollDown />
       <ShinyaigeekAnimation />
       <StarStream />
