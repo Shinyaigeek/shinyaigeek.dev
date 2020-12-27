f82cbe3ec490259328e028b4441260e836fef969
shinyaigeek <PhilisPaxil@gmail.com>
2020-12-27 15:02:31 +0900

    feature(styling): add item style

--- src/front/Home/components/Item/Item.tsx
+++ src/front/Home/components/Item/Item.tsx
@@ -3,7 +3,7 @@ import { Button } from "../../../components/Button/Button";
 
 import { getOmmit } from "../../../../server/util/getOmmit";
 import React from "react";
-import { css } from "linaria"
+import { css } from "linaria";
 export interface MetaData {
   title: string;
   description: string;
@@ -12,18 +12,48 @@ export interface MetaData {
   slug: string;
 }
 
+const itemHomeAnchor = css`
+  color: var(--theme-color);
+  text-decoration: none;
+  display: inline-block;
+
+  .title {
+    font-size: 32px;
+    text-align: center;
+    font-weight: bold;
+    padding: 8px 12px;
+  }
+`;
+
 const home = css`
   min-width: 300px;
   width: 80%;
   max-width: 750px;
   margin: 12px auto;
-  color: red;
+
+  .date {
+    color: #8fff9e;
+    padding: 16px 6px;
+    font-size: 18px;
+  }
+
+  .read--more__anchor {
+    margin: 16px 0;
+    position: absolute;
+    right: 24px;
+  }
+
+  .read--more {
+    position: relative;
+    margin: 4px 0;
+    height: 64px;
+  }
 `;
 
 export const Item = (props: MetaData) => {
   return (
     <div className={home}>
-      <a className="item--home__anchor" href={`/post/${props.slug}`}>
+      <a className={itemHomeAnchor} href={`/post/${props.slug}`}>
         <div className="title">{props.title}</div>
       </a>
       <Divider />
@@ -31,10 +61,12 @@ export const Item = (props: MetaData) => {
       <div className="tags"></div>
       <div>{getOmmit(props.description)}</div>
       <div className="read--more">
-        <a className="item--home__anchor read--more__anchor" href={`/post/${props.slug}`}>
+        <a
+          className="item--home__anchor read--more__anchor"
+          href={`/post/${props.slug}`}>
           <Button id="more-button">MORE</Button>
         </a>
       </div>
     </div>
   );
-}
+};
