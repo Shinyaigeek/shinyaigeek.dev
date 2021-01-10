8f2570e3f9df2be903911b8a3e81e376d16d849c
shinyaigeek <PhilisPaxil@gmail.com>
2021-01-10 15:22:37 +0900

    feature(github-calendar): add loading skeleton for github calendar

--- src/front/Profile/components/GitHubCalender/GitHubCalender.tsx
+++ src/front/Profile/components/GitHubCalender/GitHubCalender.tsx
@@ -1,5 +1,18 @@
+import { css } from "linaria";
 import React from "react";
+import { Loading } from "../Loading/Loading";
+
+const loadingStyle = css`
+  width: 100%;
+  height: 30vh;
+`;
 
 export const GitHubCalender = () => {
-  return <div className="ghCalender"></div>;
+  return (
+    <div className="ghCalender">
+      <div className={loadingStyle}>
+        <Loading />
+      </div>
+    </div>
+  );
 };
