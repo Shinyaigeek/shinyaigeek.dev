cd143eea3cb288232cf9a3862a8593a8cf7bcbee
shinyaigeek <philispaxil@gmail.com>
2021-07-28 17:43:15 +0900

    Feature: get dirs detail recursively

--- packages/editor/src/components/useLocalDirectory/selector.ts
+++ packages/editor/src/components/useLocalDirectory/selector.ts
@@ -4,6 +4,37 @@ import { directory } from "./atom";
 /**
  * @package
  */
+export interface Directory {
+  handler: FileSystemDirectoryHandle;
+  files: string[];
+  directories: Directory[];
+}
+
+const getDirectoryFromHandler: (
+  handler: FileSystemDirectoryHandle
+) => Promise<Directory> = async function (handler) {
+  const directory: Directory = {
+    handler,
+    files: [],
+    directories: [],
+  };
+
+  for await (const name of handler.keys()) {
+    if (name.includes(".")) {
+      directory.files.push(name);
+    } else {
+      directory.directories.push(
+        await getDirectoryFromHandler(await handler.getDirectoryHandle(name))
+      );
+    }
+  }
+
+  return directory;
+};
+
+/**
+ * @package
+ */
 export const imgDirAndArticleDir = selector({
   key: "imgDirAndArticleDir",
   get: async ({ get }) => {
@@ -17,7 +48,43 @@ export const imgDirAndArticleDir = selector({
         "articles"
       );
 
-      return [imgDirHandler, articleDirHandler] as const;
+      const imgDir: Directory = {
+        handler: imgDirHandler,
+        files: [],
+        directories: [],
+      };
+
+      const articleDir: Directory = {
+        handler: articleDirHandler,
+        files: [],
+        directories: [],
+      };
+
+      for await (const name of imgDirHandler.keys()) {
+        if (name.includes(".")) {
+          imgDir.files.push(name);
+        } else {
+          imgDir.directories.push(
+            await getDirectoryFromHandler(
+              await imgDirHandler.getDirectoryHandle(name)
+            )
+          );
+        }
+      }
+
+      for await (const name of articleDirHandler.keys()) {
+        if (name.includes(".")) {
+          articleDir.files.push(name);
+        } else {
+          articleDir.directories.push(
+            await getDirectoryFromHandler(
+              await articleDirHandler.getDirectoryHandle(name)
+            )
+          );
+        }
+      }
+
+      return [imgDir, articleDir] as const;
     }
 
     return [] as const;
--- packages/editor/src/components/useLocalDirectory/useLocalDirectory.ts
+++ packages/editor/src/components/useLocalDirectory/useLocalDirectory.ts
@@ -1,21 +1,20 @@
 import { useCallback } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { directory } from "./atom";
-import { imgDirAndArticleDir } from "./selector";
+import { Directory, imgDirAndArticleDir } from "./selector";
 
 export const useFS: () => readonly [
-  FileSystemDirectoryHandle | undefined,
-  FileSystemDirectoryHandle | undefined,
+  Directory | undefined,
+  Directory | undefined,
   () => Promise<void>
 ] = function () {
   const [_, setDirectoryHandler] = useRecoilState(directory);
-  const [imgDirHandler, articleDirHandler] =
-    useRecoilValue(imgDirAndArticleDir);
+  const [imgDir, articleDir] = useRecoilValue(imgDirAndArticleDir);
   const pickTargetDirectory = useCallback(async () => {
     const handle = await window.showDirectoryPicker();
 
     setDirectoryHandler(handle);
   }, [setDirectoryHandler]);
 
-  return [imgDirHandler, articleDirHandler, pickTargetDirectory] as const;
+  return [imgDir, articleDir, pickTargetDirectory] as const;
 };
