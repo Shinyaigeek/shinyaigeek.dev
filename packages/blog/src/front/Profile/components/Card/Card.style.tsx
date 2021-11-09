890b373d0f4355cd539c7921d6271bb72f7ce138
Shinyaigeek <Philispaxil@gmail.com>
2021-11-09 19:15:11 +0900

    Chore: fix styles in <Card />

--- packages/blog/src/front/Profile/components/Card/Card.style.tsx
+++ packages/blog/src/front/Profile/components/Card/Card.style.tsx
@@ -4,9 +4,26 @@ export const CardStyle = css`
   width: 200px;
   height: 150px;
 
+  display: inline-block;
+
+  background: #eee;
+  border: 1px solid white;
+  color: black;
+  border-radius: 6px;
+  margin: 12px 24px;
+
   img {
       width: 75px;
       height: 75px;
+      display: block;
+      margin: auto;
+  }
+
+  .title {
+    font-size: 20px;
+    padding: 2px;
+    display: inline-block;
+    margin: 2px auto;
   }
 `;
 
