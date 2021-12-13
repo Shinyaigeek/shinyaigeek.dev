ee0bbb11e4ad61f286340c7bcef8942fbffe9aa5
Shinyaigeek <Philispaxil@gmail.com>
2021-12-13 21:10:02 +0900

    Chore; linaria -> css modules in contact component

--- shinyaigeek.dev/src/client/components/Contact/Contact.tsx
+++ shinyaigeek.dev/src/client/components/Contact/Contact.tsx
@@ -1,38 +1,16 @@
 import { Button } from "../Button/Button";
 import React from "react";
-import { css } from "linaria";
-
-const contact = css`
-  width: 100%;
-  max-width: 300px;
-  margin: 18px auto;
-  text-align: center;
-  font-size: 18px;
-`;
-
-const icon = css`
-  padding-bottom: 8px;
-`;
-
-const contactAnchor = css`
-  display: block;
-  width: 100%;
-`;
-
-const sns = css`
-  margin: 0 auto;
-`;
-
-const snsAnchor = css`
-  font-size: 42px;
-  margin: 2px auto;
-`;
+import * as contact from "./Contact.module";
 
 export default function Contact() {
   return (
-    <div className={contact}>
-      <a href="/profile" className={contactAnchor}>
-        <img src={`/assets/static/icon_thatsme.png`} className={icon} alt="icon" />
+    <div className={contact.contact}>
+      <a href="/profile" className={contact.contactAnchor}>
+        <img
+          src={`/assets/static/icon_thatsme.png`}
+          className={contact.icon}
+          alt="icon"
+        />
       </a>
       しにゃい/Shinyaigeek
       <br />
@@ -40,26 +18,26 @@ export default function Contact() {
       <br />
       <Button id="thatsme--contact">Contact Me</Button>
       <div className="mySnsBox">
-        <div className={sns}>
+        <div className={contact.sns}>
           <a
             id="twitter"
             href="https://twitter.com/Shinyaigeek"
-            className={snsAnchor}>
-          </a>
+            className={contact.snsAnchor}
+          ></a>
         </div>
         <div className="sns">
           <a
             id="github"
             href="https://github.com/Shinyaigeek"
-            className={snsAnchor}>
-          </a>
+            className={contact.snsAnchor}
+          ></a>
         </div>
         <div className="sns">
           <a
             id="linkedin"
             href="https://www.linkedin.com/in/shinyaigeek/"
-            className={snsAnchor}>
-          </a>
+            className={contact.snsAnchor}
+          ></a>
         </div>
       </div>
     </div>
