b2439c0b0c1d10ce41a963389676372056c25f36
Shinyaigeek <Philispaxil@gmail.com>
2022-01-01 18:50:26 +0900

    Chore: DecorationTag

--- shinyaigeek.dev/src/client/components/DecorationTag/DecorationTag.tsx
+++ shinyaigeek.dev/src/client/components/DecorationTag/DecorationTag.tsx
@@ -1,23 +1,14 @@
 import { css } from "linaria";
 import React from "react";
+import * as tag from "./DecorationTag.module.scss";
 
 interface Props {
   tags: string[];
 }
 
-const tag = css`
-  display: flex;
-  width: 80%;
-  padding: 12px 0;
-
-  div {
-    padding: 2px 4px;
-  }
-`;
-
 export function DecorationTag({ tags }: Props) {
   return (
-    <div className={tag}>
+    <div className={tag.tag}>
       {tags.map((tag, index) => {
         return <div key={`decorationTag__${index}`}>{tag}</div>;
       })}
