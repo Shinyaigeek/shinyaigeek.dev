a84d3941969bb93dc806d7885f8cf28968791387
Shinyaigeek <Philispaxil@gmail.com>
2022-02-05 16:16:45 +0900

    Chore: move some styles into CSS Module

--- shinyaigeek.dev/src/client/components/Tag/Tag.tsx
+++ shinyaigeek.dev/src/client/components/Tag/Tag.tsx
@@ -3,24 +3,12 @@ export interface TagProps {
   slug: string;
   color: string;
 }
-import { css, cx } from "linaria";
 import React from "react";
-
-const tag = css`
-  border-radius: 4px;
-  width: min-content;
-  padding: 2px 5px;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 21px;
-  font-weight: bold;
-  margin: 12px 18px;
-  color: black !important;
-`;
+import * as tag from "./Tag.module.scss";
 
 export function Tag(props: TagProps) {
   return (
-    <a href={props.slug} className={cx(tag, props.color)}>
+    <a href={props.slug} className={tag.tag}>
       {props.child}
     </a>
   );
