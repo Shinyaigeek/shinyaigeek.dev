17d2d6b1b73a52936421c7ced076e4d579bbe076
Shinyaigeek <Philispaxil@gmail.com>
2022-02-06 22:27:13 +0900

    Chore: prepare build script for server

---
+++ webpack.config.server.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+require("dotenv").config();
+
+const isProd = process.env.NODE_ENV !== "development";
+const output = process.env.STATIC_FILE_OUTPUT || "server";
+
+module.exports = {
+  entry: {
+    main: "./shinyaigeek.dev/src/build/build.ts",
+  },
+  output: {
+    path: path.join(__dirname, output),
+    filename: isProd ? "[name].[contenthash].js" : "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".scss"],
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+              configFile: "tsconfig.json",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: true,
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      CONTENTFUL_SPACE_ID: JSON.stringify(process.env.CONTENTFUL_SPACE_ID),
+      CONTENTFUL_ACCESS_TOKEN: JSON.stringify(
+        process.env.CONTENTFUL_ACCESS_TOKEN
+      ),
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: isProd ? "styles.[contenthash].css" : "styles.css",
+    }),
+  ],
+};
