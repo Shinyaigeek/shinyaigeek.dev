037dbcb85d522b47062fe3e80c303653684cfebf
Shinyaigeek <Philispaxil@gmail.com>
2022-02-12 23:53:25 +0900

    Chore: fix webpack config

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -41,6 +41,7 @@ module.exports = {
             options: {
               modules: {
                 auto: true,
+                exportOnlyLocals: true
               },
             },
           },
