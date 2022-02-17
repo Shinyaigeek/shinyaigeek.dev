356e47c7e3dcc2af05fb97b6af7cca72b55b30e7
Shinyaigeek <Philispaxil@gmail.com>
2022-02-17 18:17:43 +0900

    Feature: handle i18n

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,6 +1,34 @@
-addEventListener('fetch', (event) => {
-  const { url } = event.request;
-  const pathname = new URL(url).pathname;
+import resolveAcceptLanguage from 'resolve-accept-language'
 
-  return event.respondWith(new Response("hi from" + pathname));
+addEventListener('fetch', (event) => {
+  const { url } = event.request
+  const { pathname, search } = new URL(url)
+
+  if (pathname.startsWith('/en')) {
+    return fetch(event.request)
+  }
+
+  if (
+    !pathname.includes('.') ||
+    !pathname.endsWith('.html') ||
+    !pathname.endsWith('/')
+  ) {
+    return fetch(event.request)
+  }
+
+  const acceptLanguage = event.request.headers.get('accept-language')
+  if (!acceptLanguage) {
+    return fetch(event.request)
+  }
+  const preferedLanguage = resolveAcceptLanguage(
+    acceptLanguage,
+    ['en-US', 'ja-JP'],
+    'ja-JP',
+  )
+
+  if (preferedLanguage === 'en-US') {
+    return Response.redirect(`https://shinyaigeek.dev/en${pathname}${search}`, 301)
+  }
+
+  return fetch(event.request)
 })
