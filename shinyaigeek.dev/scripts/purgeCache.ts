50ad38756d961202c452296200219ecec49f2b6f
Shinyaigeek <Philispaxil@gmail.com>
2022-02-28 23:57:12 +0900

    Feature: should cache module

--- shinyaigeek.dev/scripts/purgeCache.ts
+++ shinyaigeek.dev/scripts/purgeCache.ts
@@ -1,10 +1,39 @@
 import dotenv from "dotenv";
 import assert from "assert";
 import fetch from "node-fetch";
+import path from "path";
 
 const baseUrl = "https://api.cloudflare.com/client/v4/";
 //6386a36a9ac3e8c8494e8a4d43fd4f79b0956
 //5cbceaea7b728562c7efd0a00cec0d25
+
+const domains = [
+  "https://shinyaigeek.dev",
+  "https://www.shinyaigeek.dev",
+  "https://en.shinyaigeek.dev",
+  "https://ja.shinyaigeek.dev",
+];
+
+async function shouldPurge(filename: string) {
+  const remoteSource = await fetch(`https://shinyaigeek.dev${filename}`);
+  const localSourcePath = (() => {
+    if (filename.endsWith(".html")) {
+      return path.resolve(__dirname, `../../public/ja${filename}`);
+    }
+    if (filename === "sitemap.xml") {
+      return path.resolve(__dirname, `../../public${filename}`);
+    }
+    if (filename === "rss.xml") {
+      return path.resolve(__dirname, `../../public/ja${filename}`);
+    }
+
+    return path.resolve(__dirname, `../../public${filename}`);
+  })();
+}
+
+function getContentsShouldPurged() {
+  const shouldPurgeContents = [];
+}
 
 export const purgeCache = () => {
   dotenv.config();
@@ -20,7 +49,7 @@ export const purgeCache = () => {
     headers: {
       "Content-Type": "application/json",
       "X-Auth-Email": email,
-      "Authorization": `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
       files: [
@@ -31,7 +60,7 @@ export const purgeCache = () => {
     }),
   })
     .then(async (res) => {
-      console.log(res)
+      console.log(res);
       console.log(await res.json());
     })
     .catch((e) => {
