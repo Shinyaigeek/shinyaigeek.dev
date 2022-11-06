61bd3dfa857a8b9c48ce2d390c18c0029ae7a4c1
Shinyaigeek <Philispaxil@gmail.com>
2022-11-07 00:52:17 +0900

    Feature: implement basic generateOGImageFromBlogpost Module

---
+++ shinyaigeek.dev/src/generateOGImage/OGImageTemplate.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from "react";
+
+interface Props {
+    title: string,
+    width: number,
+    height: number
+}
+
+const FRAME_BOLD = 48;
+
+export const OGImageTemplate: FunctionComponent<Props> = function({ title, width, height }) {
+    return <div style={{ display: "flex" }}>
+        <div 
+        className="frame"
+        style={{
+            width: `${0.8 * width}px`,
+            height: `${0.8 * height}px`,
+            borderRadius: "36px",
+            border: `#FFE86A ${FRAME_BOLD} solid`,
+            position: "absolute",
+            top: `${0.1 * height - FRAME_BOLD}px`,
+            left: `${0.1 * width - FRAME_BOLD}px`,
+        }} />
+    </div>
+}
---
+++ shinyaigeek.dev/src/generateOGImage/generateOGImageFromBlogPost.tsx
@@ -0,0 +1,33 @@
+import satori from "satori";
+import { OGImageTemplate } from "./OGImageTemplate";
+import { Resvg } from "@resvg/resvg-js";
+
+interface Args {
+  title: string;
+}
+
+const OGImageHeight = 1080;
+const OGImageWidth = 1920;
+
+export const generateOGImageFromBlogPost: (args: Args) => Promise<Buffer> =
+  async function ({ title }) {
+    const svgData = await satori(
+      <OGImageTemplate
+        title={title}
+        width={OGImageWidth}
+        height={OGImageHeight}
+      />,
+      {
+        width: OGImageWidth,
+        height: OGImageHeight,
+        fonts: [],
+      }
+    );
+
+    const resvg = new Resvg(svgData);
+
+    const pngData = resvg.render();
+    const pngBuffer = pngData.asPng();
+
+    return pngBuffer;
+  };
