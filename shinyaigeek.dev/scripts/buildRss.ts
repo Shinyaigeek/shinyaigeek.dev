ba2594e59c862324c2c99221f1460b445963cd46
Shinyaigeek <Philispaxil@gmail.com>
2022-11-07 23:30:49 +0900

    Update: make directory for rss in public

--- shinyaigeek.dev/scripts/buildRss.ts
+++ shinyaigeek.dev/scripts/buildRss.ts
@@ -1,11 +1,16 @@
 import { writeFileSync } from "fs";
 import { getRss } from "../src/build/util/getRss";
 import path from "path";
+import fs from "fs/promises";
 
 export const buildRss = async () => {
   const languages = ["en", "ja"] as const;
   for (const language of languages) {
     const rss = await getRss(language);
+
+    await fs.mkdir(path.join(__dirname, `../../public/${language}`), {
+      recursive: true,
+    });
 
     writeFileSync(
       path.join(__dirname, `../../public/${language}/rss.xml`),
