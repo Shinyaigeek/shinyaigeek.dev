42ece92e98fd51b7e5ab92a156d2e3964ffbc94a
Shinyaigeek <Philispaxil@gmail.com>
2023-01-08 16:32:36 +0900

    Implement shellscript to deploy build into preview envieonment

---
+++ ops/deploy-preview.sh
@@ -0,0 +1,11 @@
+source ./.env
+
+mkdir ./tmp
+mkdir ./tmp/.ssh
+
+echo "$VPS_SECRET_KEY" >> ./tmp/.ssh/secret.pem
+chmod 600 ./tmp/.ssh/secret.pem
+
+scp  -i ./tmp/.ssh/secret.pem -P ${SSH_PORT} -r ./blog/public ${SSH_USER}@${SSH_HOST}:/var/www/preview/
+
+rm -rf ./tmp
