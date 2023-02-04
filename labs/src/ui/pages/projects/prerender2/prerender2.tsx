b0352c8b20248823d9c0d496d6972921d8507212
Shinyaigeek <Philispaxil@gmail.com>
2023-02-04 16:05:17 +0900

    Implement prerender2 project page

---
+++ labs/src/ui/pages/projects/prerender2/prerender2.tsx
@@ -0,0 +1,7 @@
+import React, { type FunctionComponent } from "react";
+
+interface Props {}
+
+export const Prerender2PageComponent: FunctionComponent<Props> = function () {
+  return <div>Prerender2</div>;
+};
