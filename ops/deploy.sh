1fe12ae41219b4c2255553b4b2a0dcd81fc4ef39
Shinyaigeek <Philispaxil@gmail.com>
2023-02-06 22:53:01 +0900

    Update h2o config and deploy script

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -7,5 +7,6 @@ echo "$VPS_SECRET_KEY" >> ./tmp/.ssh/secret.pem
 chmod 600 ./tmp/.ssh/secret.pem
 
 scp  -i ./tmp/.ssh/secret.pem -P ${SSH_PORT} -r ./blog/public ${SSH_USER}@${SSH_HOST}:/var/www/blog/
+scp  -i ./tmp/.ssh/secret.pem -P ${SSH_PORT} -r ./labs/dist ${SSH_USER}@${SSH_HOST}:/var/www/labs/
 
 rm -rf ./tmp
