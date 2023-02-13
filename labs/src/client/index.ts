e0476dd1cea0bcda5aad2858100f6c54356963f6
Shinyaigeek <Philispaxil@gmail.com>
2023-02-13 22:50:19 +0900

    Implement build script to build for client assets

---
+++ labs/src/client/index.ts
@@ -0,0 +1,6 @@
+import { TopPageComponent } from '../ui/pages/top/top';
+import { Prerender2PageComponent } from '../ui/pages/projects/prerender2/prerender2';
+
+// evaluate page component to extract css
+TopPageComponent;
+Prerender2PageComponent;
