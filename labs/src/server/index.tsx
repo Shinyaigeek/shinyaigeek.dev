4b095d07a2b32fd85e2a208d4f5fd45486e6d4be
Shinyaigeek <Philispaxil@gmail.com>
2023-02-13 22:37:25 +0900

    Implement top page route server

--- labs/src/server/index.tsx
+++ labs/src/server/index.tsx
@@ -1,10 +1,12 @@
 import { buildFastifyServer } from "./server";
-
-const MANIFEST_FILE_PATH = "build/server/manifest.json";
+import { TopPageRoutes } from './routes/index';
 
 (async function main() {
   const server = await buildFastifyServer();
   const port = Number(process.env.PORT) || 3000;
+
+  server.register(TopPageRoutes)
+
   await server.ready();
   server.listen(port, "0.0.0.0");
 })().catch((err) => {
--- labs/src/server/routes/index.tsx
+++ labs/src/server/routes/index.tsx
@@ -1,8 +1,14 @@
+import React from "react";
 import { type FastifyPluginAsync } from "fastify";
-import { renderToStaticMarkup } from 'react-dom/server';
+import { renderToStaticMarkup } from "react-dom/server";
+import { TopPageComponent } from "../../ui/pages/top/top";
 
-export const TopPageRoutes: FastifyPluginAsync = async function(app) {
-    app.get("/", async (req, res) => {
-        const html = renderToStaticMarkup()
-    })
-}
+export const TopPageRoutes: FastifyPluginAsync = async function (app) {
+  app.get("/", async (req, res) => {
+    const html = renderToStaticMarkup(
+      <TopPageComponent allProjects={["prerender2"]} />
+    );
+
+    res.send(html);
+  });
+};
