b6a30407fd2789469d30d473a117f75cef6526c8
Shinyaigeek <Philispaxil@gmail.com>
2023-02-13 23:21:00 +0900

    Implement Head component to show <head /<

---
+++ labs/src/ui/components/head/head.tsx
@@ -0,0 +1,14 @@
+import React, { type FunctionComponent } from "react";
+
+interface Props {}
+
+declare const $CSS_BUILT_ASSET_FILENAME: string;
+
+export const HeadComponent: FunctionComponent<Props> = function () {
+  return (
+    <head>
+      <title>Labs | shinyaigeek.dev</title>
+      <link rel="stylesheet" href={$CSS_BUILT_ASSET_FILENAME} />
+    </head>
+  );
+};
--- labs/src/ui/pages/top/top.tsx
+++ labs/src/ui/pages/top/top.tsx
@@ -1,5 +1,6 @@
 import React, { type FunctionComponent } from "react";
 import { HeaderComponent } from "../../components/header/header";
+import { HeadComponent } from "../../components/head/head";
 import styles from "./top.module.css";
 
 interface Props {
@@ -10,16 +11,19 @@ export const TopPageComponent: FunctionComponent<Props> = function ({
   allProjects,
 }) {
   return (
-    <div>
-      <HeaderComponent />
-      <main className={styles.top}>
-        <h1>
-          Labs playground for shinyaigeek's private study of web ecosystem
-        </h1>
-        {allProjects.map((project) => (
-          <a href={`/projects/${project}/`}>{project}</a>
-        ))}
-      </main>
-    </div>
+    <html>
+      <HeadComponent />
+      <body>
+        <HeaderComponent />
+        <main className={styles.top}>
+          <h1>
+            Labs playground for shinyaigeek's private study of web ecosystem
+          </h1>
+          {allProjects.map((project) => (
+            <a href={`/projects/${project}/`}>{project}</a>
+          ))}
+        </main>
+      </body>
+    </html>
   );
 };
