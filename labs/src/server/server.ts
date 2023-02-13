f4bddd903794155455b7ded9b10ebb3e20d86b31
Shinyaigeek <Philispaxil@gmail.com>
2023-02-13 23:23:47 +0900

    Serve static file from dist directory

--- labs/src/server/server.ts
+++ labs/src/server/server.ts
@@ -1,7 +1,15 @@
-import fastify from 'fastify';
+import fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import { LABS_OUTPUT_DIRECTORY } from '../../tools/build-utility';
 
-export async function buildFastifyServer(): Promise<ReturnType<typeof fastify>> {
-    const server = fastify();
+export async function buildFastifyServer(): Promise<
+  ReturnType<typeof fastify>
+> {
+  const server = fastify();
 
-    return server;
+  server.register(fastifyStatic, {
+    root: LABS_OUTPUT_DIRECTORY
+  })
+
+  return server;
 }
