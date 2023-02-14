4a3ba3241be7fedf088b701ae56d484b4751c74e
Shinyaigeek <Philispaxil@gmail.com>
2023-02-14 23:20:10 +0900

    Apply common component to prerender2 page component

--- labs/src/ui/pages/projects/prerender2/prerender2.tsx
+++ labs/src/ui/pages/projects/prerender2/prerender2.tsx
@@ -1,11 +1,24 @@
 import React, { type FunctionComponent } from "react";
+import { HeadComponent } from "../../../components/head/head";
+import { HeaderComponent } from "../../../components/header/header";
+
+import styles from "./prerender2.module.css";
+import "../../../styles/global.css";
 
 interface Props {}
 
 export const Prerender2PageComponent: FunctionComponent<Props> = function () {
   return (
-    <div>
-      Prerender2 go to <a href="/projects/prerender2/subpage">subpage</a>
-    </div>
+    <html>
+      <HeadComponent />
+      <body>
+        <HeaderComponent />
+        <main className={styles.prerender2}>
+          <div>
+            Prerender2 go to <a href="/projects/prerender2/subpage">subpage</a>
+          </div>
+        </main>
+      </body>
+    </html>
   );
 };
