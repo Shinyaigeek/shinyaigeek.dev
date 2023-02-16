3e34fe062a96f1e55131d1e26dda4cb0a7159bdc
Shinyaigeek <Philispaxil@gmail.com>
2023-02-16 22:12:44 +0900

    Setup lint staged precoit hooks

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,36 @@
+import path from 'path';
+import { ESLint } from 'eslint';
+
+const buildEslintCommand = async (filenames) => {
+    const filteredFiles = await removeIgnoredFiles(filenames);
+
+    if (filteredFiles.length < 1) {
+        return '';
+    }
+    return `eslint --fix ${filteredFiles
+        .map((f) => path.relative(process.cwd(), f))
+        .join(' ')}`;
+};
+
+const removeIgnoredFiles = async (files) => {
+    const eslint = new ESLint();
+    const isIgnored = await Promise.all(
+        files.map((file) => {
+            return eslint.isPathIgnored(file);
+        })
+    );
+    const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+    return filteredFiles;
+};
+
+const config = {
+    '*.{js,jsx,mjs,ts,tsx}': async (files) => {
+        return [
+            await buildEslintCommand(files),
+            `prettier --write ${files.map((file) => `"${file}"`).join(' ')}`,
+        ].filter((command) => command.length > 0);
+    },
+    '*.{html,md,yaml,yml,json,css,scss,less,sass,graphql,mdx}': ['prettier --write'],
+};
+
+export default config;
