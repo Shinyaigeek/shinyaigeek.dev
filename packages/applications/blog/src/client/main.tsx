fc38bbea84190f77ce10c94d611c4817811c0e73
Shinyaigeek <Philispaxil@gmail.com>
2023-02-28 22:19:50 +0900

    Evaluate root page component in client script

--- packages/applications/blog/src/client/main.tsx
+++ packages/applications/blog/src/client/main.tsx
@@ -3,7 +3,14 @@ import { registerGitHubCalendar } from './registerGitHubCalendar';
 // allow js
 // @ts-ignore
 import { messages as ja } from '../locales/ja/messages';
+import Home from './Home/Home';
+import { Profile } from './Profile/Profile';
+import Post from './Post/Post';
 i18n.load('ja', ja);
 i18n.activate('ja');
 
 registerGitHubCalendar();
+
+Home;
+Profile;
+Post;
