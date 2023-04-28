05e2d27ff3d940d7307a71b52574f1ba969d6e18
Shinyaigeek <Philispaxil@gmail.com>
2023-04-28 15:58:00 +0900

    Fix sitemap build script to handle esm

--- packages/applications/blog/scripts/buildSitemap.ts
+++ packages/applications/blog/scripts/buildSitemap.ts
@@ -1,6 +1,10 @@
 import { writeFileSync } from 'fs';
-import { getSiteMap } from '../src/build/util/getSitemap';
+import { getSiteMap } from '../src/build/util/getSitemap.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 export const buildSitemap = async () => {
     const languages = ['en', 'ja'] as const;
--- packages/applications/blog/src/build/util/getSitemap.ts
+++ packages/applications/blog/src/build/util/getSitemap.ts
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
-import { getBlogPosts } from './getBlogPosts';
+import { getBlogPosts } from './getBlogPosts.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
