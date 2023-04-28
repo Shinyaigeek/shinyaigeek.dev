1ff07ae6e0f507b50f3583356c129a407011f834
Shinyaigeek <Philispaxil@gmail.com>
2023-04-28 15:36:10 +0900

    Setup lingui swc-plugin

--- packages/modules/build-tool/javascript/swc/build-swc-config.ts
+++ packages/modules/build-tool/javascript/swc/build-swc-config.ts
@@ -3,11 +3,20 @@ import { Config } from '@swc/core';
 export const buildSwcConfig: () => Config = function () {
     return {
         jsc: {
+            parser: {
+                syntax: 'typescript',
+                tsx: true,
+            },
             transform: {
                 react: {
                     runtime: 'automatic',
                 },
             },
+            experimental: {
+                plugins: [
+                    ["@lingui/swc-plugin", {}]
+                ]
+            }
         },
     };
 };
