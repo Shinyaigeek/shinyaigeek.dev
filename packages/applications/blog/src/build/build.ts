d6a47fd6c8d17add44c74f120da8841e91bcfdff
Shinyaigeek <Philispaxil@gmail.com>
2023-08-24 02:15:09 +0900

    Fix format

--- packages/applications/blog/src/build/build.ts
+++ packages/applications/blog/src/build/build.ts
@@ -1,9 +1,7 @@
-import path from 'path';
 import { handleIndex } from './handlers/index/index';
 import { getChildren } from './handlers/post/getChildren/getChildren';
 import { handleProfile } from './handlers/profile/handleProfile';
 import { Router } from './router/router';
-import { writeFileWithDir } from './util/writeFileWithDir';
 import { minify } from 'html-minifier';
 import { addDOCTYP } from './util/addDOCTYPE';
 import { messages as ja } from '../locales/ja/messages';
