382507148b6f6240298d514ab35c3597d8cac700
Shinobu Hayashi <philispaxil@gmail.com>
2024-03-09 18:18:21 +0900

    Coppy pages components from blog workspace to turbo-blog workspace

---
+++ packages/applications/turbo-blog/src/ui/pages/Post/components/MetaInfo/MetaInfo.tsx
@@ -0,0 +1,22 @@
+// import { Edit } from "@zeit-ui/react-icons";
+
+import { DecorationTag } from '../../../components/DecorationTag/DecorationTag';
+import React from 'react';
+import metaInfo from './MetaInfo.module.css';
+
+export function MetaInfo(props: {
+    fields: {
+        tags: string[];
+        title: string;
+        publishedAt: string;
+    };
+}) {
+    const { tags } = props.fields;
+    return (
+        <div className={metaInfo.metaInfo}>
+            <h1>{props.fields.title}</h1>
+            <div>{props.fields.publishedAt}</div>
+            <DecorationTag tags={tags} />
+        </div>
+    );
+}
