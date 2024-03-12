f1daf3cb697055549916bcca5952da59fa031e6b
Shinyaigeek <Philispaxil@gmail.com>
2024-03-13 02:07:02 +0900

    Implement the router class

--- packages/modules/ssg-router/src/router/basic.ts
+++ packages/modules/ssg-router/src/router/basic.ts
@@ -5,8 +5,6 @@ export type HandlerArguments = {
 export type Handler = (args: HandlerArguments) => Promise<string>;
 
 export type BasicRouter = {
-    routing: Map<string, Handler>;
-
     on: (path: string, handler: Handler) => void;
     out: (path: string) => void;
 };
---
+++ packages/modules/ssg-router/src/router/router.ts
@@ -0,0 +1,17 @@
+import type { BasicRouter, Handler } from "./basic";
+
+export class Router implements BasicRouter {
+    private routing: Map<string, Handler> = new Map();
+    
+    on(path: string, handler: Handler) {
+        this.routing.set(path, handler);
+    }
+
+    out(path: string) {
+        const handler = this.routing.get(path);
+        if (handler) {
+            handler({ path });
+        }
+    }
+    
+}
