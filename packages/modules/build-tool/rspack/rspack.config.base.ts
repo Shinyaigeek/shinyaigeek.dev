633126c94019c725da7c26ad94766675b250a695
Shinobu Hayashi <philispaxil@gmail.com>
2024-03-15 21:54:46 +0900

    Define rspack base config in build-tool package

---
+++ packages/modules/build-tool/rspack/rspack.config.base.ts
@@ -0,0 +1,26 @@
+import { Configuration } from '@rspack/cli';
+import { buildSwcConfig } from '../javascript/swc/build-swc-config.js';
+
+export const rspackBaseConfig: Configuration = {
+    resolve: {
+        extensions: ['.ts', '.tsx', '.mts', '.cts', '.js', '.jsx', '.cjs', '.mjs', '.json', '.css'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "builtin:swc-loader",
+                options: buildSwcConfig(),
+            },
+            {
+              test: /\.css$/,
+              use: [
+                {
+                  loader: 'postcss-loader',
+                },
+              ],
+              type: 'css',
+            },
+        ],
+    },
+};
