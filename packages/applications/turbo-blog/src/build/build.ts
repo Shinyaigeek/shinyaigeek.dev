25f30c534a2676f3b43bc0e3c5a0bf3c5d2ade2e
Shinobu Hayashi <philispaxil@gmail.com>
2024-04-29 01:33:00 +0900

    Remove unnecessary router implementation

--- packages/applications/turbo-blog/src/build/build.ts
+++ packages/applications/turbo-blog/src/build/build.ts
@@ -1,12 +1,1 @@
-import { Router } from "ssg-router";
-import { generateIndexPage } from "./handlers/index/generate";
-import { outputIndexPage } from "./handlers/index/output";
-
-const router = new Router();
-
-router.on("/", {
-	generate: generateIndexPage,
-	output: outputIndexPage,
-});
-
-router.out("/");
+console.log("build");
--- packages/modules/ssg-router/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { Router } from "./router/router";
-export type { Plugin } from "./plugin/basic";
