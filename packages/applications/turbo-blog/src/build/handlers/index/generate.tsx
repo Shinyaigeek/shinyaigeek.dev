db263685a4c5015488ce201686eebac760f7e96b
Shinobu Hayashi <philispaxil@gmail.com>
2024-04-28 14:05:20 +0900

    Build pages ui with layout components

--- packages/applications/turbo-blog/src/build/handlers/index/generate.tsx
+++ packages/applications/turbo-blog/src/build/handlers/index/generate.tsx
@@ -1,6 +1,11 @@
 import { renderToStaticMarkup } from "react-dom/server";
+import { Layout } from "../../../ui/components/Layout/Layout";
 import { Home } from "../../../ui/pages/Home/Home";
 
 export const generateIndexPage: () => Promise<string> = async () => {
-	return renderToStaticMarkup(<Home />);
+	return renderToStaticMarkup(
+		<Layout language="ja" page="1" currentPath="/">
+			<Home />
+		</Layout>,
+	);
 };
--- packages/applications/turbo-blog/src/ui/components/Layout/Layout.tsx
+++ packages/applications/turbo-blog/src/ui/components/Layout/Layout.tsx
@@ -1,21 +1,19 @@
-import React from "react";
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
 import layout from "./Layout.module.css";
 
-// biome-ignore lint: reason
-export function Layout(Component: (props?: any) => JSX.Element) {
-	// biome-ignore lint: reason
-	return (props?: any) => (
+interface LayoutProps {
+	language: "ja" | "en";
+	currentPath: string;
+	page: string;
+	children: React.ReactNode;
+}
+
+export function Layout({ language, currentPath, page, children }: LayoutProps) {
+	return (
 		<div className={layout.root}>
-			<Header
-				language={props.language}
-				currentPath={props.currentPath}
-				page={props.page}
-			/>
-			<div className={layout.inner}>
-				<Component {...props} />
-			</div>
+			<Header language={language} currentPath={currentPath} page={page} />
+			<div className={layout.inner}>{children}</div>
 			<Footer />
 		</div>
 	);
