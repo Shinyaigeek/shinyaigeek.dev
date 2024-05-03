63dfb3e61ff8483026924f178ce42ac80f053f70
Shinobu Hayashi <philispaxil@gmail.com>
2024-05-03 15:53:36 +0900

    Fix the logic to get blog contents

--- packages/applications/turbo-blog/src/build/application/getBlogPosts/getBlogposts.usecase.ts
+++ packages/applications/turbo-blog/src/build/application/getBlogPosts/getBlogposts.usecase.ts
@@ -1,5 +1,6 @@
 import type { BlogContent } from "../../model/blog/blog.entity";
 import type { BlogRepository } from "../../model/blog/blog.repository";
+import { Language } from "../../model/language/language.entity";
 import { M17NContents } from "../../model/m17n/m17n.entity";
 
 export class GetBlogPostsUsecase {
@@ -7,9 +8,35 @@ export class GetBlogPostsUsecase {
 
 	async getBlogPosts(): Promise<M17NContents<BlogContent>[]> {
 		const blogPosts = await this.blogPostRepository.getBlogs();
-		const ja = blogPosts.filter((blogPost) => blogPost.language === "ja");
-		const en = blogPosts.filter((blogPost) => blogPost.language === "en");
+		const ja = blogPosts.filter(
+			(blogPost) => blogPost.language === Language.ja,
+		);
+		const en = blogPosts.filter(
+			(blogPost) => blogPost.language === Language.en,
+		);
 
-		return new M17NContents(ja, en);
+		const contents: M17NContents<BlogContent>[] = [];
+
+		for (const blogPost of ja) {
+			const existingContent = en.find(
+				(content) => content.metadata.title === blogPost.metadata.title,
+			);
+			if (existingContent) {
+				contents.push(new M17NContents(blogPost, existingContent));
+			} else {
+				contents.push(new M17NContents(blogPost, undefined));
+			}
+		}
+
+		for (const blogPost of en) {
+			const existingContent = ja.find(
+				(content) => content.metadata.title === blogPost.metadata.title,
+			);
+			if (!existingContent) {
+				contents.push(new M17NContents(undefined, blogPost));
+			}
+		}
+
+		return contents;
 	}
 }
