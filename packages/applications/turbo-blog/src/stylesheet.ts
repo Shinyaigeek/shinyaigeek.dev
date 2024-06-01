20472861168eb5351f9dff13d9fb80036ea4602d
Shinobu Hayashi <philispaxil@gmail.com>
2024-06-01 17:53:42 +0900

    Output layout component's stylesheet

--- packages/applications/turbo-blog/src/stylesheet.ts
+++ packages/applications/turbo-blog/src/stylesheet.ts
@@ -3,6 +3,8 @@
  * So we need a file which includes only pages components to extract stylesheets.
  */
 
+import { Layout } from "./ui/components/Layout/Layout";
 import { Home } from "./ui/pages/Home/Home";
 
+Layout;
 Home;
