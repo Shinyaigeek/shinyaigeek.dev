de7816b0f2f668fbfad824640e2db5e37a73fb1b
Shinobu Hayashi <philispaxil@gmail.com>
2024-06-01 18:56:07 +0900

    Refactor StarStream component to use FunctionComponent

--- packages/applications/turbo-blog/src/ui/pages/Home/components/StartStream/StarStream.tsx
+++ packages/applications/turbo-blog/src/ui/pages/Home/components/StartStream/StarStream.tsx
@@ -1,33 +1,11 @@
+import type { FunctionComponent } from "react";
 import { stars, startStreamStyle } from "./StarStream.module.css";
 
-export function StarStream() {
-	return (
-		<div className={startStreamStyle}>
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-			<div className={stars} />
-		</div>
-	);
-}
+export const StarStream: FunctionComponent = () => (
+	<div className={startStreamStyle}>
+		{[...Array(25)].map((_, i) => (
+			// biome-ignore lint/suspicious/noArrayIndexKey: index as key is ok in this context
+			<div key={i} className={stars} />
+		))}
+	</div>
+);
