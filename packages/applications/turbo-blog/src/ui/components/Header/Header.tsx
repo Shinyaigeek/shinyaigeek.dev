c55a896c8ead3b3825eab4865f5ce7b48dae8de9
Shinobu Hayashi <philispaxil@gmail.com>
2024-06-02 23:28:13 +0900

    Use FunctionComponent

--- packages/applications/turbo-blog/src/ui/components/Header/Header.tsx
+++ packages/applications/turbo-blog/src/ui/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import type { FunctionComponent } from "react";
 import { Language } from "../Language/Language";
 import {
 	active,
@@ -8,51 +9,51 @@ import {
 	title,
 } from "./Header.module.css";
 
-export function Header({
-	language,
-	currentPath,
-	page,
-}: {
+interface HeaderProps {
 	language: "en" | "ja";
 	currentPath: string;
 	page: string;
-}) {
-	return (
-		<div className={header}>
-			<div className={title}>
+}
+
+export const Header: FunctionComponent<HeaderProps> = ({
+	language,
+	currentPath,
+	page,
+}) => (
+	<div className={header}>
+		<div className={title}>
+			<a href="/" className="link2Home">
+				<div>
+					<img
+						src={"/assets/static/icon_transparent_header.png"}
+						alt="icon"
+						className={icon}
+						width="36px"
+						height="36px"
+					/>
+					shinyaigeek.dev
+				</div>
+			</a>
+		</div>
+		<div className={contents}>
+			<div className={anchor}>
+				<Language currentLanguage={language} currentPath={currentPath} />
+			</div>
+			<div className={`${anchor} ${page === "home" ? active : ""}`}>
 				<a href="/" className="link2Home">
-					<div>
-						<img
-							src={"/assets/static/icon_transparent_header.png"}
-							alt="icon"
-							className={icon}
-							width="36px"
-							height="36px"
-						/>
-						shinyaigeek.dev
-					</div>
+					Blog
 				</a>
 			</div>
-			<div className={contents}>
-				<div className={anchor}>
-					<Language currentLanguage={language} currentPath={currentPath} />
-				</div>
-				<div className={`${anchor} ${page === "home" ? active : ""}`}>
-					<a href="/" className="link2Home">
-						Blog
-					</a>
-				</div>
-				<div className={`${anchor} ${page === "profile" ? active : ""}`}>
-					<a href="/profile" id="link2profile">
-						Profile
-					</a>
-				</div>
-				<div className={`${anchor}`}>
-					<a href="https://github-activity.shinyaigeek.dev/" id="link2activity">
-						Activity
-					</a>
-				</div>
+			<div className={`${anchor} ${page === "profile" ? active : ""}`}>
+				<a href="/profile" id="link2profile">
+					Profile
+				</a>
+			</div>
+			<div className={`${anchor}`}>
+				<a href="https://github-activity.shinyaigeek.dev/" id="link2activity">
+					Activity
+				</a>
 			</div>
 		</div>
-	);
-}
+	</div>
+);
