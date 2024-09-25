e2512259bf4b438c14000af59da5af4b12dea2d2
Shinobu Hayashi <philispaxil@gmail.com>
2024-09-25 12:51:20 +0900

    Remove unnecessary components

--- packages/applications/turbo-blog/src/ui/components/ShinyaigeekAnimation/ShinyaigeekAnimation.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Shinyaigeek } from "../ShinyaigeekPortrait/ShinyaigeekPortrait";
-export function ShinyaigeekAnimation() {
-	return (
-		<div>
-			<Shinyaigeek />
-		</div>
-	);
-}
