771a0c68654ae32672ec69346a6d60cfa3b420ee
Shinyaigeek <philispaxil@gmail.com>
2025-02-24 11:42:06 +0900

    fmt

--- packages/applications/turbo-blog/src/ui/components/about-me/about-me.stories.tsx
+++ packages/applications/turbo-blog/src/ui/components/about-me/about-me.stories.tsx
@@ -1,11 +1,19 @@
 import type { Story } from "@ladle/react";
-import { AboutMe } from "./about-me";
 import { LanguageContext } from "../../../../context/language-context";
+import { AboutMe } from "./about-me";
 
 export const AboutMeJaStory: Story = function () {
-	return <LanguageContext.Provider value="ja"><AboutMe /></LanguageContext.Provider>
+	return (
+		<LanguageContext.Provider value="ja">
+			<AboutMe />
+		</LanguageContext.Provider>
+	);
 };
 
 export const AboutMeEnStory: Story = function () {
-	return <LanguageContext.Provider value="en"><AboutMe /></LanguageContext.Provider>
+	return (
+		<LanguageContext.Provider value="en">
+			<AboutMe />
+		</LanguageContext.Provider>
+	);
 };
